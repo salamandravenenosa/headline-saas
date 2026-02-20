f47fa32e89078786e69d395a21498e697692cabf
Salamandra Venenosa <salamandravenenosa1@gmail.com>
2026-02-20 17:42:51 -0300

    fix: remove deprecated serverActions config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // Enable server-side only for API
-    experimental: {
-        serverActions: true,
-    },
 };
 
 export default nextConfig;
